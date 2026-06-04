bdb86e15ea815a3ae8724918a9c78e551167c6c6
Ervireb <Ervin.Rebane@tptlive.ee>
2026-06-04 20:56:38 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import HeaderNav from './containers/HeaderNav/HeaderNav';
+
+class App extends Component {
+  render() {
+    return (
+      <HeaderNav/>
+    );
+  }
+}
+
+export default App;
